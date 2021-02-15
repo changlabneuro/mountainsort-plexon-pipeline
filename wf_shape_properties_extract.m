9bbfa01010e5beaf4fba29e1625c35cf487888c0
Beamlak Ashenafi <beam@Beamlaks-MBP.wireless.yale.internal>
2021-02-15 17:09:10 -0500

    update clustering and properties extractor

--- wf_shape_properties_extract.m
+++ wf_shape_properties_extract.m
@@ -4,6 +4,7 @@ peak_distance = [];
 frequency = [];
 imaxs_ss = [];
 templates = [];
+file_names = [];
 while true
   thisline = fgetl(fid);
   if ~ischar(thisline); break; end  %end of file
@@ -16,37 +17,29 @@ while true
     wf = wvout.wf;
   end
   nCh = size(wf{1},2);
-  nUnits_recovered = numel(wf);
+  nUnits_recovered = numel(wvout.maxchn);
   nUnits_recovered_id = unique(spktime(3,:));
   cnt = 0;
   pkts = {};
   mfrs = {};
   imaxs = {};
-  tmpss = {};
-  for j = 1:(nUnits_recovered)  
-     hs = [];
-     tmp_tmp = {};
-     for i = 1:nCh
-       idx_real = find(wvout.maxchn==i-1);  
-       cnt = cnt+1;  
-       unit_wv = squeeze(wf{j}(:,i,:));
-       template = mean(zscore(unit_wv,0,2));  
-       tmp_tmp{i} = template;
-     end
-     [~, i_varmax] = max(cellfun(@var,tmp_tmp));  
-     [peak, i2] = min(tmp_tmp{i_varmax});  
-     [trough, i1] = max(tmp_tmp{i_varmax}(i2:end));
-     pkt = [abs(i2 - i1) i2 i1];
+  tmps = {};
+  for j = 1:(nUnits_recovered)
+     tmp_tmp = wvout.templates(j, wvout.maxchn(j) + 1, :);
+     [peak, i2] = max(tmp_tmp);  
+     [trough, i1] = min(tmp_tmp(i2:end));
+     pkt = [peak trough peak-trough i2 i1 abs(i2 - i1)];
      mfr = length(find(spktime(3,:) == nUnits_recovered_id(j)))/ (spktime(2,end)/40000);
      pkts{j} = pkt;
      mfrs{j} = mfr;
      imaxs{j} = i_varmax;
-     tmpss{j} = tmp_tmp;
+     tmps{j} = tmp_tmp;
    end
    peak_distance{idx} = pkts;
    frequency{idx} = mfrs;
    imaxs_ss{idx} = imaxs;
-   templates{idx} = tmpss;
+   templates{idx} = tmps;
+   file_names{idx} = thisline;
    idx = idx + 1;
 end
-fclose('pl2_used.txt');
+fclose('all');
